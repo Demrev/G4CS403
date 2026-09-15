a0a7c88bd7eedb4bfe2daedd5db73ba9cd20c6a5
Demrev <demmadrazo@gmail.com>
2026-09-15 17:50:33 +0800

    Activity 5

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try {
-        // Test database connection
+        
         await pool.query("SELECT 1");
 
         console.log("Database connected successfully");
